64efe71662e0305b2a19a553920d8392532a68c8
Neko <cronhound@gmail.com>
2017-11-20 13:07:18 +1030

    add login method

--- includes/controllers/UserController.php
+++ includes/controllers/UserController.php
@@ -14,4 +14,33 @@ class UserController extends Controller {
 			self::not_found();
 		}
 	}
+
+	public static function login() {
+		global $db;
+
+		$errors = array();
+
+		if ( empty( $_POST['username'] ) ) {
+			$errors[] = "You must enter a username";
+		}
+		if ( empty( $_POST['password'] ) ) {
+			$errors[] = "You must enter a password";
+		}
+
+		if ( empty( $errors ) ) {
+			$user = $db->table( 'users' )->select()
+		    ->where( 'username', $_POST['username'] )
+		    ->get();
+
+		  if ( !$user || !password_verify( $_POST['password'], $user[0]['password'] ) ) {
+		  	$errors[] = 'Invalid username or password';
+		  }
+		}
+
+	  if ( !empty( $errors ) ) {
+	  	self::response( array( 'errors' => $errors ) );
+	  } else {
+	  	self::response( new User( $user[0] ) );
+	  }
+	}
 }
--- routes.php
+++ routes.php
@@ -4,4 +4,6 @@ $router = new Router( function( FastRoute\RouteCollector $r ) {
 	$r->addRoute( 'GET', '/topics/{slug}', 'TopicController::view' );
 	$r->addRoute( 'GET', '/categories/{slug}', 'CategoryController::view' );
 	$r->addRoute( 'GET', '/users/{slug}', 'UserController::view' );
+
+	$r->addRoute( 'POST', '/login', 'UserController::login' );
 });
