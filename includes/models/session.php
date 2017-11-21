8eec3fa6ef09f71ac9caea992a31a44584597d94
Neko <cronhound@gmail.com>
2017-11-21 17:02:33 +1030

    made sessions belong to users

--- includes/models/session.php
+++ includes/models/session.php
@@ -8,11 +8,15 @@ class Session extends Illuminate\Database\Eloquent\Model {
 
 	public function save( array $options = array() ) {
 		if( empty( $this->sid ) ) {
-    	$this->sid = md5( random_bytes( 50 ) );
+			$this->sid = md5( random_bytes( 50 ) );
 		}
 
 		$this->time = time();
 
 		return parent::save( $options );
+	}
+
+	public function user() {
+		return $this->belongsTo( 'User', 'uid' );
 	}
 }
