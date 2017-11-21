f7140d0771b4631f3f1444e966f7df587b6abdf6
Neko <cronhound@gmail.com>
2017-11-21 14:22:19 +1030

    hid data that the api doesn't need from user output

--- includes/models/user.php
+++ includes/models/user.php
@@ -3,6 +3,9 @@
 class User extends Illuminate\Database\Eloquent\Model {
 	protected $primaryKey = 'uid';
 
+	protected $hidden = array( 'email', 'password', 'salt',
+		'loginkey', 'avatartype', 'regip', 'lastip', 'usergroup', 'additionalgroups', 'lastvisit', 'lastpost', 'website', 'icq', 'aim', 'yahoo', 'skype', 'google', 'birthday', 'birthdayprivacy', 'allownotices', 'hideemail', 'subscriptionmethod', 'invisible', 'receivefrombuddy', 'pmnotice', 'pmnotify', 'buddyrequestspm', 'buddyrequestsauto', 'threadmode', 'showimages', 'showvideos', 'showsigs', 'showavatars', 'showquickreply', 'showredirect', 'ppp', 'tpp', 'daysprune', 'dateformat', 'timeformat', 'timezone', 'dst', 'dstcorrection', 'buddylist', 'ignorelist', 'style', 'away', 'awaydate', 'returndate', 'awayreason', 'pmfolders', 'notepad', 'referrer', 'referrals', 'reputation', 'language', 'timeonline', 'showcodebuttons', 'totalpms', 'unreadpms', 'warningpoints', 'moderateposts', 'moderationtime', 'suspendposting', 'suspensiontime', 'suspendsignature', 'suspendsigtime', 'coppauser', 'classicpostbit', 'loginattempts', 'usernotes', 'sourceeditor', 'groupposts', 'groupsin', 'numgroupsin' );
+
 	public function replies() {
 		return $this->hasMany( 'Reply', 'uid' );
 	}
