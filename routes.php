698766dbce9b03b4b50aceef698f882da96feccf
Neko <cronhound@gmail.com>
2017-11-22 12:22:24 +1030

    added versioning to routes

--- routes.php
+++ routes.php
@@ -1,9 +1,13 @@
 <?php
 
-$router = new Router( function( FastRoute\RouteCollector $r ) {
-	$r->addRoute( 'GET', '/topics/{slug}', 'TopicController::view' );
-	$r->addRoute( 'GET', '/categories/{slug}', 'CategoryController::view' );
-	$r->addRoute( 'GET', '/users/{slug}', 'UserController::view' );
+use FastRoute\RouteCollector;
 
-	$r->addRoute( 'POST', '/login', 'UserController::login' );
+$router = new Router( function( RouteCollector $r ) {
+	$r->addGroup('/v0', function ( RouteCollector $r ) {
+		$r->addRoute( 'GET', '/topics/{slug}', 'TopicController::view' );
+		$r->addRoute( 'GET', '/categories/{slug}', 'CategoryController::view' );
+		$r->addRoute( 'GET', '/users/{slug}', 'UserController::view' );
+
+		$r->addRoute( 'POST', '/login', 'UserController::login' );
+	});
 });
