80ccfc692911b14c3fe0fcc5e0879baa97bccadb
Neko <cronhound@gmail.com>
2017-11-22 12:18:13 +1030

    added requires login method to controller

--- includes/controllers/controller.php
+++ includes/controllers/controller.php
@@ -12,4 +12,13 @@ class Controller {
 		header( 'HTTP/1.0 404 Not Found' );
 		self::response( array( 'errors' => array( 404 => 'Page Not Found' ) ) );
 	}
+
+	static function requires_login() {
+		global $session;
+
+		if ( !$session || !$session->id ) {
+		header( 'HTTP/1.1 401 Unauthorized' );
+		self::response( array( 'errors' => array( 401 => 'Unauthorized' ) ) );
+		}
+	}
 }
